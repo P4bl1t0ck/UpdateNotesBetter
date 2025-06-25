e7600e65276fd95f7842027359b123e85e440e49
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-25 02:04:52 -0500

    Cambios

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,3 +1,6 @@
+using Notes.Models;
+using Notes.ViewModels;
+
 namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
@@ -6,4 +9,13 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
+    string noteId = query["saved"].ToString();
+    NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+// If note is found, update it
+if (matchedNote != null)
+{
+    matchedNote.Reload();
+    AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+}
 }
