227a9882c78406a5e1b225e96e08fe6cd74432d9
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-25 02:07:51 -0500

    Actualizacione shechas al codigo

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,5 +1,6 @@
 using Notes.Models;
 using Notes.ViewModels;
+using static Android.App.DownloadManager;
 
 namespace Notes.Views;
 
@@ -18,4 +19,34 @@ if (matchedNote != null)
     matchedNote.Reload();
     AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
 }
+else
+    AllNotes.Insert(0, new NoteViewModel(Models.Note.Load(noteId)));
+
+}
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+{
+    if (query.ContainsKey("deleted"))
+    {
+        string noteId = query["deleted"].ToString();
+        NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+        // If note exists, delete it
+        if (matchedNote != null)
+            AllNotes.Remove(matchedNote);
+    }
+    else if (query.ContainsKey("saved"))
+    {
+        string noteId = query["saved"].ToString();
+        NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+        // If note is found, update it
+        if (matchedNote != null)
+        {
+            matchedNote.Reload();
+            AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+        }
+        // If note isn't found, it's new; add it.
+        else
+            AllNotes.Insert(0, new NoteViewModel(Models.Note.Load(noteId)));
+    }
 }
