7cfc2f4595ac614339a1b0bffae6d0af19e5d4dd
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-25 14:21:42 -0500

    Reparaciones finales de el tutorial, tambien resulto errores que se presentarion durante la copilación.

--- Notes/ViewModels/NoteViewModel.cs
+++ Notes/ViewModels/NoteViewModel.cs
@@ -1,10 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Windows.Input;
 
