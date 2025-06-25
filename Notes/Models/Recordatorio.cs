4d399c4be855cac9b600f876373450c5a136dd5f
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-25 02:11:51 -0500

    Falta crear la funcion que serialize la informacion a un .json.

---
+++ Notes/Models/Recordatorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class Recordatorio
+    {
+        public string Texto { get; set; }
+        public TimeSpan FechaHora { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
