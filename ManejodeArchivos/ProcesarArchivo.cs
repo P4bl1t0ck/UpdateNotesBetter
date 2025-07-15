ce0db6186c70c30425059743b812d2ccad29090e
WilsonegasSoftware <wilson.egas@udla.edu.ec>
2025-07-15 18:32:16 -0500

    Manejo de archivos corregido

---
+++ ManejodeArchivos/ProcesarArchivo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ProcesarArchivo()
+{
+	string rutaArchivo = "/app/data/entrada.txt";
+
+	if (File.Exists(rutaArchivo)){
+		string contenido = File.ReadAllText(rutaArchivo);
+	    Console.WriteLine("Contenido Orginal");
+		Console.WriteLine(contenido);
+
+		string rutaSalida = "/app/data/salida.txt";
+	    File.WriteAllText(rutaSalida, contenido.ToUpper());
+		Console.WriteLine("Archivo procesado correctamente");
+
+}
+else
+{
+
+	Console.WriteLine("Archivo no encontrado")
+}
